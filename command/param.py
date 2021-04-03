652772728013460587bfaf56fe4dd07a5be884bd
lavadk <a@a.a>
2021-04-03 21:55:35 +0300

    params handling improved

--- command/param.py
+++ command/param.py
@@ -3,15 +3,17 @@ import argparse
 
 
 class CommandParam:
-    def __init__(self, name: str, help_str: str, param_type: Any):
+    def __init__(self, name: str, help_str: str, param_type: Any, nargs=None):
         assert name.startswith('--')
 
         self.name = name
         self.help_str = help_str
         self.param_type = param_type
+        self.nargs = nargs
 
     def add_to_parser(self, parser: argparse.ArgumentParser):
         parser.add_argument(self.name,
-                            type=str,
+                            type=self.param_type,
+                            nargs=self.nargs,
                             metavar='',
                             help=self.help_str)
