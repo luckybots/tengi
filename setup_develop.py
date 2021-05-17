8a2e9f34be7afffc218dab8cb5e01ee0ec72c98c
lavadk <a@a.a>
2021-05-17 16:31:03 +0300

    setup file for development lib install

---
+++ setup_develop.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tengi',
+    version='dev',
+    url='',
+    author='dev',
+    author_email='',
+    description='',
+    packages=find_packages(),    
+    install_requires=[],
+)
